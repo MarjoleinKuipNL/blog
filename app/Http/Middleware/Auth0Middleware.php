0479d95e7c1327245bfb0dcf54b3b78f4ce1f8ab
rozemarijn56 <marjoleinkuip@hotmail.com>
2022-12-22 14:28:42 +0100

    first commit

---
+++ app/Http/Middleware/Auth0Middleware.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Auth0\SDK\Exception\InvalidTokenException;
+use Auth0\SDK\Helpers\JWKFetcher;
+use Auth0\SDK\Helpers\Tokens\AsymmetricVerifier;
+use Auth0\SDK\Helpers\Tokens\TokenVerifier;
+
+class Auth0Middleware {
+    /**
+     * Run the request filter.
+     * 
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $scopeRequired = null){
+        $token = $request->baererToken();
+        if(!$token){
+            return response()->json('No token provided', 401);
+        }
+
+        $decodedToken = $this->validateToken($token);
+
+        if($scopeRequired && !$this->tokenHasScope($decoedeToken, $scopeRequired)) {
+            return response()->json(['message' => 'Insufficient scope'], 403);
+        }
+
+        return $next($request);
+    }
+
+    public function validateToken($token){
+        try{
+            $jwksUri = env('AUTH0_DOMAIN') . 'well-known/jwks.json';
+            $jwksFetcher = new JWKFetcher(null, ['base_uri' => $jwksUri]);
+            $signatureVerifier = new AsymmetricVerifier($jwksFetcher);
+            $tokenVerifier = new TokenVerifier(env('AUTH0_DOMAIN'), env('AUTH0_AUD'), $signatureVerifier);
+
+            $decoded = $tokenVerifier->verify($token);
+        }catch(InvalidTokenException $e){
+            throw $e;
+        }
+    }
+    /**
+     * Check if a token has a specific scope.
+     * 
+     * @param \stdClass $token - JWT access token to check.
+     * @param string $scopeRequired  $scopeRequired - Scope to check for.
+     * 
+     * @return bool
+     */
+    protected function tokenHasScope($token, $scopeRequired){
+        if(empty($token['scope'])){
+            return false;
+        }
+
+        $tokenScopes = explode(' ', $token['scope']);
+        return in_array($scopeRequired, $tokenScopes);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+//use App\Http\Controllers\AuthorController;
+/** @var \Laravel\Lumen\Routing\Router $router */
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
+
+
+$router->group(['prefix' => 'api', 'middleware' => 'auth'], function () use ($router){
+    $router->get('authors', ['uses' => 'Author Controller@showAllAuthors']);
+    $router->get('authors/{id}', ['uses' => 'AuthorController@showOneAuthors']);
+    $router->post('authors', ['middleware' => 'auth:create:authors','uses' => 'AuthorController@create']);
+    $router->delete('authors/{id}', ['uses' => 'AuthorController@delete']);
+    $router->put('authors/{id}', ['uses' => 'AuthorController@update']);
+});
